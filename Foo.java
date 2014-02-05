313134a39fdd496d92b351c0cdf7f38b41e10415
151levym <151levym@student.dist113.org>
2014-02-05 10:03:48 -0600

    Update Foo.java

--- Foo.java
+++ Foo.java
@@ -3,6 +3,7 @@ public class Foo
   public static void main(String[] args)
   {
     System.out.println("HI");
-    System.out.println("Did this work")
+    System.out.println("Did this work");
+    
   }
 }
